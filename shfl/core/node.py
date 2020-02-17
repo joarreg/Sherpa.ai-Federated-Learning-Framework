9c2ae3ce177bf433cf161c14023aadc586923dd0
gegose <g.gonzalez@sherpa.ai>
2020-02-17 12:12:58 +0100

    Adding test cases to core package

--- shfl/core/node.py
+++ shfl/core/node.py
@@ -1,3 +1,6 @@
+from shfl.core.data import LabeledData
+
+
 class DataNode:
     """
     This class represents an independent data node
@@ -72,6 +75,8 @@ class DataNode:
             String identifying the private data to use for this model
         """
         labeled_data = self._private_data.get(training_data_key)
+        if not hasattr(labeled_data, 'data') or not hasattr(labeled_data, 'label'):
+            raise ValueError("Private data needs to have 'data' and 'label' to train a model")
         self._model.train(labeled_data.data, labeled_data.label)
 
     def predict(self, data):
--- test/core/test_data.py
+++ test/core/test_data.py
@@ -1,55 +1,5 @@
 import numpy as np
-import pytest
-
-import shfl.core.data
-from shfl.core.data import FederatedData, LabeledData
-from shfl.core.query import Get
-from shfl.core.node import DataNode
-
-
-def test_federate_array():
-    data_size = 10000
-    num_clients = 1000
-    array = np.random.rand(data_size)
-    federated_array = shfl.core.data.federate_array("my_array1", array, num_clients)
-    assert federated_array.num_nodes() == num_clients
-    assert federated_array.identifier == "my_array1"
-
-
-def test_federate_array_size_private_data():
-    data_size = 10000
-    num_clients = 10
-    array = np.random.rand(data_size)
-    federated_array = shfl.core.data.federate_array("my_array", array, num_clients)
-    for data_node in federated_array:
-        assert len(data_node.query_private_data(Get(), "my_array")) == data_size/num_clients
-
-    assert federated_array[0].query_private_data(Get(), "my_array")[0] == array[0]
-
-
-def test_federated_data():
-    data_size = 10
-    federated_data = FederatedData("my_federated_data")
-    assert federated_data.num_nodes() == 0
-    array = np.random.rand(data_size)
-    federated_data.add_data_node(DataNode(), array)
-    assert federated_data.num_nodes() == 1
-    assert federated_data[0].query_private_data(Get(), "my_federated_data")[0] == array[0]
-
-
-def test_federated_data_identifier():
-    data_size = 10
-    federated_data = FederatedData("my_federated_data")
-    array = np.random.rand(data_size)
-    federated_data.add_data_node(DataNode(), array)
-    with pytest.raises(KeyError):
-        federated_data[0].query_private_data(Get(), "bad_identifier_federated_data")
-
-
-def test_federated_data_repeated_identifier():
-    my_data = FederatedData("my_federated_data")
-    with pytest.raises(ValueError):
-        FederatedData("my_federated_data")
+from shfl.core.data import LabeledData
 
 
 def test_labeled_data():
