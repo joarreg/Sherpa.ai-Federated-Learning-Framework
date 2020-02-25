f4dd188e93c64b44c974ecc912f5ef262d4ca084
Nuria Rodríguez <rbnuria6@gmail.com>
2020-02-25 15:51:58 +0100

    Changes in order to comply coding style

--- test/federated_aggregator/test_weighted_fedavg_aggregator.py
+++ test/federated_aggregator/test_weighted_fedavg_aggregator.py
@@ -1,16 +1,15 @@
 import numpy as np
-import pytest
 
 from shfl.federated_aggregator.weighted_avgfed_aggregator import WeightedAvgFedAggregator
 
 def test_aggregated_weights():
     num_clients = 10
     num_layers = 5
-    tams = [[128,64],[64,64],[64,64],[64,32],[32,10]]
+    tams = [[128, 64], [64, 64], [64, 64], [64, 32], [32, 10]]
 
     weights = []
     for i in range(num_clients):
-        weights.append([np.random.rand(tams[j][0],tams[j][1]) for j in range(num_layers)])
+        weights.append([np.random.rand(tams[j][0], tams[j][1]) for j in range(num_layers)])
 
     clients_params = np.array(weights)
 
