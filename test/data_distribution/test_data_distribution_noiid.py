e55d3c743ff89bf8eb6dc13bef94b8883738e54e
gegose <g.gonzalez@sherpa.ai>
2020-02-26 15:23:29 +0100

    Fixing broken test

--- test/data_distribution/test_data_distribution_noiid.py
+++ test/data_distribution/test_data_distribution_noiid.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 
 from shfl.data_base.data_base import DataBase
-from shfl.data_distribution.data_distribution_non_iid import NonIidDataDistribution, choose_labels
+from shfl.data_distribution.data_distribution_non_iid import NonIidDataDistribution
 from shfl.core.query import Get
 
 
@@ -14,16 +14,20 @@ class TestDataBase(DataBase):
         self._train_data = np.random.rand(250).reshape([50, 5])
         self._test_data = np.random.rand(250).reshape([50, 5])
         self._validation_data = np.random.rand(250).reshape([50, 5])
-        self._train_labels = np.random.randint(0, 10, 50)
-        self._test_labels = np.random.randint(0, 10, 50)
-        self._validation_labels = np.random.randint(0, 10, 50)
+        self._train_labels = np.random.randint(0, 3, 50)
+        self._test_labels = np.random.randint(0, 3, 50)
+        self._validation_labels = np.random.randint(0, 3, 50)
 
 
 def test_choose_labels():
     num_nodes = 3
     total_labels = 10
 
-    random_labels = choose_labels(num_nodes, total_labels)
+    data = TestDataBase()
+    data.load_data()
+    data_distribution = NonIidDataDistribution(data)
+
+    random_labels = data_distribution.choose_labels(num_nodes, total_labels)
     all_labels = np.concatenate(random_labels)
 
     for node in random_labels:
@@ -64,14 +68,15 @@ def test_make_data_federated():
     for data in all_data:
         idx.append(np.where((data == train_data).all(axis=1))[0][0])
 
-    seed_weights = [6, 12, 5]
+    seed_weights = [30, 15, 15]
     for i, weight in enumerate(weights):
         assert federated_data[i].shape[0] == seed_weights[i]
 
-    assert all_data.shape[0] == 23
+    assert all_data.shape[0] == 60
     assert num_nodes == federated_data.shape[0] == federated_label.shape[0]
     assert (np.sort(all_data.ravel()) == np.sort(train_data[idx, ].ravel())).all()
     assert (np.sort(all_label) == np.sort(train_label[idx])).all()
+
 
 def test_get_federated_data():
     data = TestDataBase()
