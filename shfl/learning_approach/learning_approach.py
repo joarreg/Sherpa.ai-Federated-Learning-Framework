5430fa9fe0ba5cf306be8ba142c011827ae9bce9
Goran <g.stipcich@sher.pa>
2020-05-07 18:41:43 +0200

    Federated models: Added option to apply data access policy to client's model parameters in Federated Government and added notebook for K-means clustering

--- shfl/learning_approach/learning_approach.py
+++ shfl/learning_approach/learning_approach.py
@@ -9,13 +9,16 @@ class LearningApproach(abc.ABC):
         model_builder: Function that return a trainable model (see: [Model](../../model))
         federated_data: Federated data to use. (see: [FederatedData](../../private/federated_operation/#federateddata-class))
         aggregator: Federated aggregator function (see: [Federated Aggregator](../../federated_aggregator))
+        model_param_access: Policy to access model's parameters, by default non-protected (see: [DataAccessDefinition](../data/#dataaccessdefinition)) 
     """
-    def __init__(self, model_builder, federated_data, aggregator):
+    def __init__(self, model_builder, federated_data, aggregator, model_params_access=None):
         self._federated_data = federated_data
         self._model = model_builder()
         self._aggregator = aggregator
         for data_node in federated_data:
             data_node.model = model_builder()
+            if model_params_access is not None:
+                data_node.configure_model_params_access(model_params_access)
 
     @abc.abstractmethod
     def train_all_clients(self):
