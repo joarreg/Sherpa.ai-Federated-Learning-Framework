cba8a8a4b349faf0ab76b9fc9c316de5e4c233c7
josebummer <jantonioruiz@correo.ugr.es>
2020-05-27 03:18:16 +0200

    100% coverage
    100% coverage

--- test/data_distribution/test_data_distribution_iid.py
+++ test/data_distribution/test_data_distribution_iid.py
@@ -75,3 +75,43 @@ def test_make_data_federated():
     assert num_nodes == federated_data.shape[0] == federated_label.shape[0]
     assert (np.sort(all_data.ravel()) == np.sort(train_data[idx,].ravel())).all()
     assert (np.sort(all_label) == np.sort(train_label[idx])).all()
+
+
+def test_make_data_federated_wrong_weights():
+    data = TestDataBase()
+    data.load_data()
+    data_distribution = IidDataDistribution(data)
+
+    train_data, train_label = data_distribution._database.train
+    validation_data, validation_label = data_distribution._database.validation
+
+    train_data = np.concatenate((train_data, validation_data), axis=0)
+    train_label = np.concatenate((train_label, validation_label), axis=0)
+
+    num_nodes = 3
+    percent = 60
+    # weights = np.full(num_nodes, 1/num_nodes)
+    weights = [0.5, 0.5, 0.5]
+    federated_data, federated_label = data_distribution.make_data_federated(train_data,
+                                                                            train_label,
+                                                                            num_nodes,
+                                                                            percent,
+                                                                            weights)
+
+    weights = np.array([float(i) / sum(weights) for i in weights])
+    data_distribution.get_federated_data(3)
+
+    all_data = np.concatenate(federated_data)
+    all_label = np.concatenate(federated_label)
+
+    idx = []
+    for data in all_data:
+        idx.append(np.where((data == train_data).all(axis=1))[0][0])
+
+    for i, weight in enumerate(weights):
+        assert federated_data[i].shape[0] == int(weight * int(percent * train_data.shape[0] / 100))
+
+    assert all_data.shape[0] == int(percent * train_data.shape[0] / 100)
+    assert num_nodes == federated_data.shape[0] == federated_label.shape[0]
+    assert (np.sort(all_data.ravel()) == np.sort(train_data[idx,].ravel())).all()
+    assert (np.sort(all_label) == np.sort(train_label[idx])).all()
