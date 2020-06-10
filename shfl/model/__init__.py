8d93b0425a8cde793cd3400381b16fd9c66a53d9
rbnuria <rbnuria6@gmail.com>
2020-06-10 11:04:55 +0200

    Added clustering to aggregation operators

--- shfl/model/__init__.py
+++ shfl/model/__init__.py
@@ -5,3 +5,5 @@ from __future__ import print_function
 from shfl.model.deep_learning_model import DeepLearningModel
 from shfl.model.linear_regression_model import LinearRegressionModel
 from shfl.model.model import TrainableModel
+from shfl.model.kmeans_model import KMeansModel
+
