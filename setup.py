8850684a0bcbfdcbb4ba2bbc37466c12b8bea482
gegose <g.gonzalez@sherpa.ai>
2020-06-17 16:39:34 +0200

    Improving setup file

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name="shfl", packages=find_packages(), install_requires=['numpy', 'emnist', 'scikit-learn', 'pytest',
-                                                               'tensorflow>=2.2.0', 'scipy', 'six', 'pathlib2'])
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name="shfl",
+      version="0.1.0",
+      description="Sherpa.FL is an open-source framework for Machine Learning that is dedicated to data privacy "
+                  "protection",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/sherpaai/Sherpa.FL",
+      packages=find_packages(),
+      install_requires=['numpy', 'emnist', 'scikit-learn', 'pytest', 'tensorflow>=2.2.0', 'scipy', 'six', 'pathlib2'],
+      python_requires='>=3.7')
